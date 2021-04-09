4c0d172c70b10b6211dbcb4165f39e720ba91c74
Ladislau2020 <75049562+Ladislau2020@users.noreply.github.com>
2021-04-10 00:38:35 +0100

    Calculadora Javascript
    arquivo em javascript da calculadora

---
+++ Cal.js
@@ -0,0 +1,16 @@
+function val(result){
+form.disp.value = form.disp.value + result;
+}
+
+function cal(){
+	if(form.disp.value == ""){
+		alert("please enter numbers")
+	}else{
+	form.disp.value = eval(form.disp.value);
+	}
+}
+
+var btn = form.equal;
+btn.addEventListener('dblclick', function (){
+	form.disp.value = "";
+});
